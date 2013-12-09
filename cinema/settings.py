797b99fafbbbdae24c8f1cf9c4339dc393e6d2c6
Rafael Cunha de Almeida <rafael@kontesti.me>
2013-12-09 03:53:22 +0000

    Initial version

---
+++ cinema/settings.py
@@ -0,0 +1,15 @@
+# Scrapy settings for cinema project
+#
+# For simplicity, this file contains only the most important settings by
+# default. All the other settings are documented here:
+#
+#     http://doc.scrapy.org/en/latest/topics/settings.html
+#
+
+BOT_NAME = 'cinema'
+
+SPIDER_MODULES = ['cinema.spiders']
+NEWSPIDER_MODULE = 'cinema.spiders'
+
+# Crawl responsibly by identifying yourself (and your website) on the user-agent
+#USER_AGENT = 'cinema (+http://www.yourdomain.com)'
